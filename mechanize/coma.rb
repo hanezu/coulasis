d8433309345dfaf2e0a02a083f8c682431bb9a96
hanezu <tim.zhc@gmail.com>
2017-03-18 16:59:51 +0900

    First usable version

--- mechanize/coma.rb
+++ mechanize/coma.rb
@@ -2,17 +2,29 @@ require 'json'
 
 class Coma
 
-  def initialize(name, day, time)
-	@name = name
-    @day = day
-    @time = time
+  def initialize(data)
+	@data = data
   end
 
-  def self.parse name, time_str
-    parse_result = self.parse_weekday time_str
+  def self.parse page, url
+	time_str = self.find_time page
+	if time_str != nil
+	  time_parse_result = self.parse_weekday time_str
+	else
+	  time_parse_result = ['','']
+	end
+	name = self.find_name page
+	if name == nil
+	  name = ''
+	end
 	coma_arr = []
-	parse_result.each do |parse_param|
-	  coma_arr << Coma.new(name.strip, parse_param[0], parse_param[1])
+	time_parse_result.each do |parse_param|
+	  coma_arr << Coma.new({
+	    name: name.strip,
+	    day: parse_param[0],
+		time: parse_param[1],
+		url: url
+	  })
     end
 	coma_arr
   end
@@ -27,11 +39,19 @@ class Coma
     buffer
   end
 
+  def self.find_time page
+	[*page.search('td'), nil].each_cons(2) do |t, t_next|
+	  return t_next.text if t.text == '(曜時限)'
+	end
+  end
+
+  def self.find_name page
+	[*page.search('td'), nil].each_cons(2) do |t, t_next|
+	  return t_next.text if t.text == '(科目名)'
+	end
+  end
+
   def to_hash
-	{
-	  name: @name,
-	  day: @day,
-	  time: @time
-	}
+	@data
   end
 end
--- mechanize/main.rb
+++ mechanize/main.rb
@@ -3,22 +3,18 @@ require 'mechanize'
 require 'json'
 require './coma'
 
-def find_time page
-  [*page.search('td'), nil].each_cons(2) do |t, t_next|
-    return t_next.text if t.text == '(曜時限)'
-  end
-end
-
-def find_name page
-  [*page.search('td'), nil].each_cons(2) do |t, t_next|
-    return t_next.text if t.text == '(科目名)'
-  end
-end
-
 def get_login_info 
   buffer = []
-  File.open('login.txt').each do |line|
+  File.open('../login.txt').each do |line|
 	buffer << line.split(':')[1].strip
+  end
+  buffer
+end
+
+def get_course_urls
+  buffer = []
+  File.open("../course_urls.txt").each do |line|
+	buffer << line unless line =~ /^ *#/
   end
   buffer
 end
@@ -33,13 +29,13 @@ kyo_form.j_password = password
 middle_page = agent.submit(kyo_form, kyo_form.buttons.first)
 homepage = middle_page.form.click_button
 
+# url = 'https://www.k.kyoto-u.ac.jp/student/u/s/syllabus/detail?no=3958'
 coma_buffer = []
-
-search1 = agent.get('https://www.k.kyoto-u.ac.jp/student/u/s/syllabus/detail?no=3958')
-time_str = find_time search1
-name = find_name search1
-comas = Coma.parse(name, time_str)
-coma_buffer += comas
+get_course_urls.each do |url|
+  course_page = agent.get(url)
+  comas = Coma.parse(course_page, url)
+  coma_buffer += comas
+end
 
 coma_json = coma_buffer.map(&:to_hash).to_json
 File.open("../assets/data.json", "w") do |f|
