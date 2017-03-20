fc20e7bdab1ed191bedb297642d9817167ae2e4f
hanezu <tim.zhc@gmail.com>
2017-03-20 22:54:00 +0900

    Complete version for courses of 理学部 and 工学部.

--- mechanize/coma.rb
+++ mechanize/coma.rb
@@ -1,57 +1,89 @@
 require 'json'
 
 class Coma
-
-  def initialize(data)
-	@data = data
-  end
-
   def self.parse page, url
-	time_str = self.find_time page
-	if time_str != nil
-	  time_parse_result = self.parse_weekday time_str
-	else
-	  time_parse_result = ['','']
-	end
-	name = self.find_name page
-	if name == nil
-	  name = ''
-	end
-	coma_arr = []
-	time_parse_result.each do |parse_param|
-	  coma_arr << Coma.new({
-	    name: name.strip,
-	    day: parse_param[0],
-		time: parse_param[1],
-		url: url
-	  })
+    url.strip!
+
+    new_coma = -> subcoma do
+      coma_times = subcoma.find_time_in page
+      name = subcoma.find_name_in page
+	  puts name.strip
+      coma_arr = []
+      coma_times.each do |parse_param|
+        coma_arr << {
+            name: name.strip,
+            day: parse_param[0],
+            time: parse_param[1],
+            url: url
+        }
+      end
+      coma_arr
     end
-	coma_arr
+
+    if url.start_with? "https://www.k.kyoto-u.ac.jp/student/u/s/syllabus/"
+      new_coma.call ScienceComa
+    elsif url.start_with? "http://www.t.kyoto-u.ac.jp/syllabus-s/"
+      new_coma.call EngineeringComa
+    else
+      raise ParseComaError, "No according coma with url #{url}"
+    end
+  end
+  
+  def self.find(page, str)
+	[*page.search('td'), nil].each_cons(2) do |t, t_next|
+	  return t_next.text if t.text == str
+	end
+	raise ParseComaError, "find #{str} failed"
   end
 
-  def self.parse_weekday time_str
+
+end
+
+class ScienceComa < Coma
+
+  def self.find_time_in page
+    time_str = Coma.find(page, '(曜時限)')
     datetimes = time_str.split(',')
     buffer = []
     datetimes.each do |datetime|
-	  datetime.strip!
+      datetime.strip!
       buffer << [datetime[0], datetime[1]]
     end
     buffer
   end
 
-  def self.find_time page
-	[*page.search('td'), nil].each_cons(2) do |t, t_next|
-	  return t_next.text if t.text == '(曜時限)'
-	end
+  def self.find_name_in page
+    Coma.find(page, '(科目名)')
   end
 
-  def self.find_name page
-	[*page.search('td'), nil].each_cons(2) do |t, t_next|
-	  return t_next.text if t.text == '(科目名)'
-	end
+end
+
+class EngineeringComa < Coma
+
+  def self.find_time_in page
+    time_str = nil
+    page.search('tr').each do |t|
+      table_head = t.search('th')[0]
+      if table_head != nil && table_head.text == '曜時限'
+        time_str = t.search('td')[0].text
+      end
+    end
+    if time_str == nil
+      raise ParseComaError, "find #{str} failed"
+    end
+    buffer = []
+    splitted_time = time_str.split('・')
+    buffer << [splitted_time[0].strip[0], splitted_time[1].strip[0]]
   end
 
-  def to_hash
-	@data
+  def self.find_name_in page
+    page.search('h2').each do |t|
+      return t.text.split(':')[-1].strip if t.text =~ /授業科目名/
+    end
+    raise ParseComaError, "find name failed"
   end
+end
+
+class ParseComaError < StandardError
+
 end
--- mechanize/main.rb
+++ mechanize/main.rb
@@ -3,10 +3,11 @@ require 'mechanize'
 require 'json'
 require './coma'
 
+
 def get_login_info 
   buffer = []
   File.open('../login.txt').each do |line|
-	buffer << line.split(':')[1].strip
+    buffer << line.split(':')[1].strip
   end
   buffer
 end
@@ -14,7 +15,7 @@ end
 def get_course_urls
   buffer = []
   File.open("../course_urls.txt").each do |line|
-	buffer << line unless line =~ /^ *#/ or line.strip.length == 0
+    buffer << line unless line =~ /^ *#/ or line.strip.length == 0
   end
   buffer
 end
@@ -29,7 +30,6 @@ kyo_form.j_password = password
 middle_page = agent.submit(kyo_form, kyo_form.buttons.first)
 homepage = middle_page.form.click_button
 
-# url = 'https://www.k.kyoto-u.ac.jp/student/u/s/syllabus/detail?no=3958'
 coma_buffer = []
 get_course_urls.each do |url|
   course_page = agent.get(url)
@@ -37,7 +37,8 @@ get_course_urls.each do |url|
   coma_buffer += comas
 end
 
-coma_json = coma_buffer.map(&:to_hash).to_json
+coma_json = coma_buffer.to_json
 File.open("../assets/data.json", "w") do |f|
   f.write coma_json
 end
+
